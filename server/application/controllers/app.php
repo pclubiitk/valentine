5a033fcc71723338d57f96a4876c7b7f0562bef4
root <root@pclub.in>
2014-02-12 08:36:22 +0530

    prefinal

--- server/application/controllers/app.php
+++ server/application/controllers/app.php
@@ -17,31 +17,90 @@
 			}
 			else{
 				$this->session->set_userdata('user_id', $user['rollno']);
+				$this->session->set_userdata('gender', $user['gender']);
 				header('Location: http://pclub.in/valentine/home.html');
-				echo 'loggedIn';
+				//echo $user;
 			}
 		}
-	
+			
 		public function typeahead(){
 			$term = $this->input->get('term');
 			if($this->session->userdata('user_id')){
 				$matches = $this->user_model->searchString($term);
-				$result = '[';
-				foreach($matches as $row){
-				if($row->publickey == '')
-					$publickey = 'AAAAB3NzaC1yc2EAAAADAQABAAAAgQC9m+LxWdtRRsz3KXxIlhxbyqTR3onWoLzu2+Ka2ThUMFV5WZC9nxGmjKAmB81KY5vOmRSEzTHESxCZckgDqdzXeYqOynnxXxFN8kd0Voqep2kUu/Q8t5ENCDftYjRt63QY6JU0KB9jhQLaIyGctNrU5/5BlgmZgfTS5olood24Nw==';
-				else
-					$publickey = $row->publickey;
-				$result = $result."{'name':'".$row->name."','rollno':'".$row->rollno."','publickey':'".$publickey."','Department':'CSE'},";
+				for($row=0; $row<sizeof($matches); $row++){
+					if($matches[$row]->publickey == '')
+						$matches[$row]->publickey = 'AAAAB3NzaC1yc2EAAAADAQABAAAAgQC9mdefault+LxWdtRRsz3KXxIlhxbyqTR3onWoLzu2+Ka2ThUMFV5WZC9nxGmjKAmB81KY5vOmRSEzTHESxCZckgDqdzXeYqOynnxXxFN8kd0Voqep2kUu/Q8t5ENCDftYjRt63QY6JU0KB9jhQLaIyGctNrU5/5BlgmZgfTS5olood24Nw==';
 				}
-				$result = $result."]";
-				echo $result;
-			}
-		
+				$this->output->set_header('Content-Type: application/json; charset=utf-8');
+				echo json_encode($matches);
+			}		
 		}
-		public function add($data){
+
+		public function crushes(){
+			if($this->session->userdata('user_id')){
+				$rollno = $this->session->userdata('user_id');
+				$list = $this->user_model->getCrushes($rollno);
+				$this->output->set_header('Content-Type: application/json; charset=utf-8');
+				echo json_encode($list);
+			}
+		}
+		public function details(){
+				$rollno = $this->input->get('rollno');
+                $res = $this->user_model->details($rollno);
+                if($res[0]->publickey == '')
+					$res[0]->publickey = 'AAAAB3NzaC1yc2EAAAADAQABAAAAgQC9mdefault+LxWdtRRsz3KXxIlhxbyqTR3onWoLzu2+Ka2ThUMFV5WZC9nxGmjKAmB81KY5vOmRSEzTHESxCZckgDqdzXeYqOynnxXxFN8kd0Voqep2kUu/Q8t5ENCDftYjRt63QY6JU0KB9jhQLaIyGctNrU5/5BlgmZgfTS5olood24Nw==';
+                $this->output->set_header('Content-Type: application/json; charset=utf-8');
+				echo json_encode($res);
+
+        }
+        public function mydetails(){
+        	if($this->session->userdata('user_id')){
+        		$rollno = $this->session->userdata('user_id');
+        		$res = $this->user_model->details($rollno);
+        		$this->output->set_header('Content-Type: application/json; charset=utf-8');
+					echo json_encode($res);
+        	}
+        }
+        public function logout(){
+        	$this->session->sess_destroy();
+        	header('Location: http://pclub.in/valentine/index.html');
+        }
+		public function addto(){
+			$rollno[1] = $this->input->post('rollno1');
+			$rollno[2] = $this->input->post('rollno2');
+			$rollno[3] = $this->input->post('rollno3');
+			$rollno[4] = $this->input->post('rollno4');
+			$data[1] = $this->input->post('data1');
+			$data[2] = $this->input->post('data2');
+			$data[3] = $this->input->post('data3');
+			$data[4] = $this->input->post('data4');
+			if($this->session->userdata('user_id')){
+				if($this->user_model->checkfrom($this->session->userdata('user_id')))
+					$this->user_model->addto($rollno, $data);
+			}
+		}
+		public function addfrom(){
+			if($this->session->userdata('user_id')){
+				$rollno = $this->session->userdata('user_id');
+				if($this->user_model->checkfrom($rollno)){
+					$data = $this->input->post('data');
+					$this->user_model->addfrom($rollno, $data);
+				}
+			}
+		}
+
+		public function count(){
+			if($this->session->userdata('user_id')){
+				$res = $this->user_model->getCount($this->session->userdata('user_id'));
+				echo $res;
+			}
+		}
+
+		public function loggedin(){
 			if($this->session->userdata('user_id'))
-				$this->user_model->addData($data);
+				echo "TRUE";
+			else
+				echo "FALSE";
 		}
 
 }
--- server/application/models/user_model.php
+++ server/application/models/user_model.php
@@ -8,31 +8,70 @@ class user_model extends CI_Model {
         }
         public function login($username, $password, $publickey){
                 $ftp_server="webhome.cc.iitk.ac.in";
-                // set up basic connection
+                //set up basic connection
                 $conn_id = ftp_connect($ftp_server);
 
                 // login with username and password
                 $login_result = ftp_login($conn_id, $username, $password);
                 ftp_close($conn_id);
                 if($login_result){
-			$this->db->where('email',$username.'@iitk.ac.in');
-			$this->db->update('publickey', $publickey);
-                        $query = $this->db->get_where('users',array('email'=>$username.'@iitk.ac.in'));
+			$query = $this->db->where('email',$username.'@iitk.ac.in')->get('users');
                         $row = $query->row_array();
-			if($row['visitflag'] == 0){
-				$this->db->where('email',$username.'@iitk.ac.in');
-				$this->db->update('visitflag', 1);
+			$this->db->set('publickey', $publickey);
+                        $this->db->where('email',$username.'@iitk.ac.in');
+                        $this->db->update('users');
+                        return $row;
 			}
-			return $row;
-                }
                 else{
                         return FALSE;
                 }
         }
-
-        public function addData($data){
-                $this->db->insert('to', array('rollno'=>$this->session->userdata('user_id'),'data'=>$data));
+	
+	public function searchString($term){
+                $oppositeGender = $this->session->userdata['gender'] == 'F' ? 'M' : 'F';
+                $this->db->where('gender', $oppositeGender);
+		$this->db->like('name',$term,'both');
+		$query = $this->db->get('users', 20);
+		return $query->result();
+	}
+        public function addfrom($rollno, $data){
+                $this->db->insert('from', array('rollno'=>$rollno,'data'=>$data));
         }
+
+        public function details($rollno){
+                $res = $this->db->where('rollno', $rollno)->get('users');
+                return $res->result();
+        }
+        public function getCrushes($rollno){
+                $res = $this->db->where('rollno', $rollno)->get('from');
+                return $res->result();
+        }
+        public function addto($rollno, $data){
+                if($data[1] != null)
+                        $this->db->insert('to', array('rollno' => $rollno[1], 'data' => $data[1]));
+                if($data[2] != null)
+                        $this->db->insert('to', array('rollno' => $rollno[2], 'data' => $data[2]));
+                if($data[3] != null)
+                        $this->db->insert('to', array('rollno' => $rollno[3], 'data' => $data[3]));
+                if($data[4] != null)
+                        $this->db->insert('to', array('rollno' => $rollno[4], 'data' => $data[4]));
+        }
+
+        public function checkfrom($rollno){
+                $result = $this->db->where('rollno', $rollno)->get('from')->result();
+                if(sizeof($result) == 0)
+                        return TRUE;
+                else
+                        return FALSE;
+        }
+
+        public function getCount($rollno){
+                $this->db->where('rollno', $rollno);
+                $this->db->from('to');
+                return $this->db->count_all_results();
+        }
+
+
 }
 
 ?>
